3b5d07c1ab56e9d041537c4c0ca1e07be55a9a5f
Yu Feng <rainwoodman@gmail.com>
2017-08-15 15:50:13 -0700

    add test for ordereddict.

--- test_orderedobject.py
+++ test_orderedobject.py
@@ -1,4 +1,5 @@
 from orderedobject import orderedobject
+from orderedobject import ordereddict
 from orderedobject import OrderedClass
 import pytest
 
@@ -46,6 +47,26 @@ def test_pickle():
     o2 = pickle.loads(s)
     assert o2.a == o.a
     assert o2.b == o.b
+
+def test_ordereddict():
+    gc.collect()
+
+    o = ordereddict()
+
+    l = []
+    w = []
+    o['b'] = make_removable('b', l, w)
+    o['a'] = make_removable('a', l, w)
+
+    print("Before deleting", l)
+    del o # trigger deletion
+    print("After deleting", l)
+    gc.collect() # forcefully with GC.
+    print("After gc", l)
+
+    # assert reversed order.
+    assert l[0] == 'a'
+    assert l[1] == 'b'
 
 @pytest.mark.xfail(reason="See https://github.com/cython/cython/issues/1821")
 def test_orderedclass():
